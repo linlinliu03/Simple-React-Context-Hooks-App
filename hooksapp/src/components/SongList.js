d205e98a3f40aaf85092e42fdfdd67cad2121534
linlinliu <linlinliu03@gmail.com>
2020-05-05 23:03:54 -0700

    Setup hooks

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: 'almost home', id: 1 },
+        { title: 'memory gospel', id: 2 },
+        { title: 'this wild darkness', id: 3 }
+    ])
+
+    const addSong = () => {
+        setSongs([...songs, { title: "new song", id: Math.floor((Math.random() * 100) + 1)}])
+    }
     return(
        <div className="song-list">
           <ul>
-            <li>this wild darkness</li>
-            <li>memory gospel</li>
+            {songs.map(song => {
+                return (<li key={song.id}>{song.title}</li>)
+            })}
           </ul>
+          <button onClick={addSong}>Add a new song</button>
        </div>
     )
 }
